094738865fbfc84b66792e875f7d04438ca67f0f
gavinhousley <gavinhousley@gmail.com>
2026-02-06 14:32:56 +0000

    Add tests for topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,7 +19,39 @@ describe("GET /api/topics/", () => {
       .expect(200)
       .then(({ body }) => {
         const { topics } = body;
-        expect(typeof topics).toBe("object");
+        expect(topics.length).toBeGreaterThan(0);
+        expect(topics).toBeDefined();
+        topics.forEach((topic) => {
+          expect(typeof topic.description).toBe("string");
+          expect(typeof topic.slug).toBe("string");
+        });
+      });
+  });
+});
+
+describe("GET /api/articles/", () => {
+  test("Get: 200 = Responds with a list of articles", () => {
+    return request(app)
+      .get("/api/articles/")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        articles.forEach((articles) => {
+          expect(typeof articles.title).toBe("string");
+          expect(typeof articles.author).toBe("string");
+          expect(typeof articles.votes).toBe("number");
+        });
+      });
+  });
+  test("Get: 200 = Responds with an object with the key of articles and the value of an array of article objects without the body property", () => {
+    return request(app)
+      .get("/api/articles/")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        articles.forEach((articles) => {
+          expect(typeof articles.body).toBe("undefined");
+        });
       });
   });
 });
