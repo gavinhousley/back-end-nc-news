aea638b1ed320a6427f69cdd7a4c4ec7b16dd84d
gavinhousley <gavinhousley@gmail.com>
2026-02-20 16:12:29 +0000

    updated connection details

--- db/connection.js
+++ db/connection.js
@@ -4,12 +4,25 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool();
-
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 } else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
+  console.log(`Connected to database`);
 }
 
-module.exports = db;
+// const db = new Pool();
+
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+// if (!process.env.PGDATABASE) {
+//   throw new Error("No PGDATABASE configured");
+// } else {
+//   console.log(`Connected to ${process.env.PGDATABASE}`);
+// }
+
+module.exports = new Pool(config);
