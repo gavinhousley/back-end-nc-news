e682d49f59d3e721ea7dded7c29770f694ca16b3
gavinhousley <gavinhousley@gmail.com>
2026-02-20 16:15:03 +0000

    Cleaned up the connection.js

--- db/connection.js
+++ db/connection.js
@@ -6,8 +6,6 @@ require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
-} else {
-  console.log(`Connected to database`);
 }
 
 // const db = new Pool();
