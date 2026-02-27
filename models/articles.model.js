223180162aeb5f001f9976d1c8a175b21b65e72c
gavinhousley <gavinhousley@gmail.com>
2026-02-27 15:03:37 +0000

    update single article searcy query to feature comment count as with articles query

--- models/articles.model.js
+++ models/articles.model.js
@@ -22,7 +22,23 @@ exports.fetchAllArticles = (sort_by = "created_at", order = "DESC") => {
 
 exports.fetchArticleById = (article_id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
+    .query(
+      `SELECT 
+        articles.author,
+        articles.title,
+        articles.article_id,
+        articles.body,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        COUNT(comments.comment_id) ::INT AS comment_count
+     FROM articles
+     LEFT JOIN comments ON comments.article_id = articles.article_id 
+     WHERE articles.article_id = $1
+     GROUP BY articles.article_id`,
+      [article_id],
+    )
     .then(({ rows }) => rows[0]);
 };
 
