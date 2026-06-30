489c5f946f7a65ffad08f3a75d13b49bb2737547
gavinhousley <gavinhousley@gmail.com>
2026-07-01 00:13:30 +0100

    add a rate limit to the ammount of requests available in a short space of time

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const rateLimit = require("express-rate-limit");
 const topicsRouter = require("./routes/topics.routes");
 const articlesRouter = require("./routes/articles.routes");
 const usersRouter = require("./routes/users.routes");
@@ -8,18 +8,20 @@ const commentsRouter = require("./routes/comments.routes");
 
 const app = express();
 
-app.use(cors());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: { msg: "Too many requests, please try again later" },
+});
 
+app.use(cors());
 app.use(express.json());
+app.use(limiter);
 
 app.use("/api", express.static("public"));
-
 app.use("/api/topics", topicsRouter);
-
 app.use("/api/articles", articlesRouter);
-
 app.use("/api/users", usersRouter);
-
 app.use("/api/comments", commentsRouter);
 
 const {
